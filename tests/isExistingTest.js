1358a9fb8db6e8427213a33d12056747fa48b4f8
Edward Chao <EdwardChao0194@gmail.com>
2020-05-06 20:23:45 -0700

    isExisiting Part2 + isVisible + hasFocus Part 1

--- tests/isExistingTest.js
+++ tests/isExistingTest.js
@@ -5,23 +5,42 @@ beforeEach(function() {
 describe('Test whether hidden elements exist', function() {
 	
 	it('Button display is set to non display but still exists in html dom therefore should return true', function(done) {
-	#not-displayed
+
+		var isEnabled = browser.isExisting('#not-displayed');
+		console.log(isEnabled);
+		expect(isEnabled).to.equal(true);
 	});
 
 	it('Button display is set to visibility hidden but still exists in html dom therefore should return true', function(done) {
-	#visibility-hidden
+
+		var isEnabled = browser.isExisting('#visibility-hidden');
+		console.log(isEnabled);
+		expect(isEnabled).to.equal(true);
+
 	});
 
 	it('Button display is set to zero opacity but still exists in html dom therefore should return true', function(done) {
-	#zero-opacity
+
+		var isEnabled = browser.isExisting('#zero-opacity');
+		console.log(isEnabled);
+		expect(isEnabled).to.equal(true);
+
 	});
 
 	it('Header text is visible and exists in the dom therefore should return true', function(done) {
-	h1
+
+		var isEnabled = browser.isExisting('h1');
+		console.log(isEnabled);
+		expect(isEnabled).to.equal(true);
+
 	});
 
 	it('There is no such element with the id of #no-such-element within the html dom therefore should return false', function(done) {
-	#no-such-element
+
+		var isEnabled = browser.isExisting('#no-such-element');
+		console.log(isEnabled);
+		expect(isEnabled).to.equal(false);
+
 	});
 
 });
