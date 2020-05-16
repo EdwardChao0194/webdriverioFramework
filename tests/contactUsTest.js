e184858434ece112ede56efba3de6b5a4bed8848
Edward Chao <EdwardChao0194@gmail.com>
2020-05-16 12:16:13 -0700

    POM Phase 2 Part 4-6, POM Phase 3 Part 1-3

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,4 +1,4 @@
-var ContactUs_Page = require("./pageObjects/ContactUs_Page.js");
+var ContactUs_Page = require("../pageObjects/ContactUs_Page.js");
 
 beforeEach(function() {
 	browser.url('/Contact-Us/contactus.html');
@@ -6,45 +6,14 @@ beforeEach(function() {
 
 describe('Test Contact Us form WebdriverUni', function() {
 
-	function setFirstName(firstName){
-		return browser.setValue(firstNameSelector, firstName);
-	};
-	function setLastName(lastName){
-		return browser.setValue(lastNameSelector, lastName);
-	};
-	function setEmailAddress(emailAddress){
-		return browser.setValue(emailAddressSelector, emailAddress);
-	};
-	function setComments(comments){
-		return browser.setValue(commentsSelector, comments);
-	};
-	function clickSubmitButton(){
-		return browser.click(submitButtonSelector);
-	};
-
-	function confirmSuccessfulSubmission(){
-		var validateSubmissionHeader = browser.waitUntil(function() {
-			return browser.getText(successfulSubmissionSelector) == 'Thank You for your Message!';
-		}, 3000)
-		expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
-	};
-	function confirmUnsuccessfulSubmission(){
-		var validateSubmissionHeader = browser.waitUntil(function() {
-			return browser.getText(unsuccessfulSubmissionSelector) == 'Error: all fields are required';
-		}, 3000)
-		expect(browser.getText(unsuccessfulSubmissionSelector)).to.include('Error: all fields are required');
-	};
-
-	contactusDetails.forEach(function (contactusDetail){
-		it('Should be able to submit a successful submission via contact us form', function(done){ 
-			setFirstName('joe');
-			setLastName('blogs');
-			setEmailAddress(contactusDetail.email);
-			setComments(contactusDetail.body);
-			clickSubmitButton();
-			confirmSuccessfulSubmission();
-		}); 
-	});
+	it('Should be able to submit a successful submission via contact us form', function(done){ 
+		setFirstName('joe');
+		setLastName('blogs');
+		setEmailAddress('joeblogs123@outlook.com');
+		setComments('How are you?');
+		clickSubmitButton();
+		confirmSuccessfulSubmission();
+	}); 
 
 	it('Should not be able to submit a successful submission via contact us form as all fields are required(no comment)', function(done){ 
 		setFirstName('mike');
