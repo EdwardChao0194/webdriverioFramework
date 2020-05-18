85f9e94ec8b6692b019e44319fcad9e31427f9c9
Edward Chao <EdwardChao0194@gmail.com>
2020-05-18 14:00:09 -0700

    Allure Reports + Attaching images, Deleting Files and Folders

--- tests/ajaxClickTest.js
+++ tests/ajaxClickTest.js
@@ -1,8 +1,8 @@
 describe("Test that the button is clickable once the Ajax loader completes loading", function(){
 
-	it.skip("Attempt to click the button asap", function(done){
+	it("Attempt to click the button asap", function(done){
 		browser.url('/Ajax-Loader/index.html');
-		browser.click('#button1')
+		browser.click('#button1555')
 	});
 
 	it("Attempt to click the button after 7 secounds", function(done){
