10a01f98cea3603d61a238fb3bddc14a8ea32689
Edward Chao <EdwardChao0194@gmail.com>
2020-05-24 15:54:09 -0700

    Update/Download additional dependencies, Foundations, Modify Test for Async Await, and Customize POM Commands

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,32 +1,16 @@
 var ContactUs_Page = require("../pageObjects/ContactUs_Page.js");
 
-beforeEach(function() {
-	browser.url('/Contact-Us/contactus.html');
+beforeEach(async() => {
+	await browser.url('/Contact-Us/contactus.html');
 });
 
-describe('Test Contact Us form WebdriverUni', function() {
+describe('Test Contact Us form WebdriverUni', () => {
 
-	it('Should be able to submit a successful submission via contact us form', function(done){ 
+	it('Test 1: Should be able to submit a successful submission via contact us form', () => { 
 		ContactUs_Page.submitAllInformationViaContactUsForm('joe','blogs','joeblogs123@outlook.com','how are you?');
 	}); 
 
-	it('Should not be able to submit a successful submission via contact us form as all fields are required(no comment)', function(done){ 
-		ContactUs_Page.setFirstName('mike');
-		ContactUs_Page.setLastName('woods');
-		ContactUs_Page.setEmailAddress('mikewoods@mail.com');
-		ContactUs_Page.clickSubmitButton();
-		ContactUs_Page.confirmUnsuccessfulSubmission();		
-	});
-
-	it('Should not be able to submit a successful submission via contact us form as all fields are required(no last_name and comment)', function(done){ 
-
-		ContactUs_Page.setFirstName('sarah');
-		ContactUs_Page.setEmailAddress('sarah@mail.com');
-		ContactUs_Page.clickSubmitButton();
-		ContactUs_Page.confirmUnsuccessfulSubmission();
-	});
-
-	it('Should not be able to submit a successful submission via contact us form as all fields are required(no email and comment)', function(done){ 
+	it('Test 2: Should not be able to submit a successful submission via contact us form as all fields are required(no email and comment)', () => { 
 		ContactUs_Page.setLastName('jones');
 		ContactUs_Page.setEmailAddress('jones@mail.com')
 		ContactUs_Page.clickSubmitButton();
